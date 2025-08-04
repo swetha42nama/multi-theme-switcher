5f0ab93d15ca342e20017c0ae3cfb2fa57820842
swetha42nama <swetha42nama@gmail.com>
2025-08-04 07:04:27 +0530

    clean code

--- src/App.js
+++ src/App.js
@@ -1,27 +1,4 @@
 
-// import React from "react";
-// import { Routes, Route } from "react-router-dom";
-// import Layout from "./components/Layout";
-// import Home from "./pages/Home";
-// import About from "./pages/About";
-// import Contact from "./pages/Contact";
-// import { ThemeProvider } from "./context/ThemeContext";
-
-// const App = () => {
-//   return (
-//     <ThemeProvider>
-//       <Layout>
-//         <Routes>
-//           <Route path="/" element={<Home />} />
-//           <Route path="/about" element={<About />} />
-//           <Route path="/contact" element={<Contact />} />
-//         </Routes>
-//       </Layout>
-//     </ThemeProvider>
-//   );
-// };
-
-// export default App;
 
 import React from "react";
 import { Routes, Route } from "react-router-dom";
--- src/index.js
+++ src/index.js
@@ -1,22 +1,9 @@
 
-// import React from "react";
-// import ReactDOM from "react-dom/client";
-// import App from "./App";
-// import { BrowserRouter } from "react-router-dom";
-
-// const root = ReactDOM.createRoot(document.getElementById("root"));
-// root.render(
-//   <BrowserRouter>
-//     <App />
-//   </BrowserRouter>
-// );
-// src/index.js
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
-import "./index.css"; // Tailwind or your global styles
-
+import "./index.css"; 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <BrowserRouter>
